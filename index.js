849c04099c4e20fb5c307c07fda29c3bf700fafc
Dudás Ádám <adam.dudas@banzaicloud.com>
2019-09-24 16:56:44 +0200

    change naming and update URL on param change

--- index.js
+++ index.js
@@ -1,74 +1,176 @@
-const textEncoder = new TextEncoder('utf-8');
+const textEncoder = new TextEncoder('utf-8')
 
 function arrayBufferToBase64(buffer) {
-    return base64js.fromByteArray(new Uint8Array(buffer));
+    return base64js.fromByteArray(new Uint8Array(buffer))
 }
 
-function computeUniqpass(password, site, limit) {
-    if (password === "" || site === "") {
-        return Promise.resolve("");
-    } else {
-        return crypto.subtle.digest('SHA-256', textEncoder.encode(password + site))
+const defaultLimit = 44;
+
+function computeUniqpass(secret, subject, limit) {
+    if (secret !== "" && subject !== "") {
+        return crypto.subtle.digest('SHA-256', textEncoder.encode(secret + subject))
             .then(arrayBufferToBase64)
-            .then(pass => pass.substring(0, limit || undefined));
+            .then(pass => pass.substring(0, limit || undefined))
+    }
+    return Promise.resolve("")
+}
+
+const secretStorageKey = "secret"
+
+function loadValuesFromStorage(controls) {
+    const storedSecret = localStorage.getItem(secretStorageKey)
+    if (storedSecret !== null) {
+        controls.secret.set(storedSecret)
+        controls.rememberSecret.set(true)
     }
 }
 
-const passwordStorageKey = "password";
+function loadValuesFromURL(controls) {
+    const params = new URLSearchParams(location.search)
+
+    if (params.has('limit')) {
+        const limitString = params.get('limit')
+        const limitValue = parseInt(limitString)
+        if (isNaN(limitValue)) {
+            console.warn("'limit' parameter set to invalid value:", limitString)
+        } else {
+            controls.limit.set(limitValue)
+        }
+    }
+    const subjectValue = params.get('subject')
+    if (subjectValue) {
+        controls.subject.set(subjectValue)
+    }
+}
+
+const variable = function() {
+
+    const make = ({get, set, listen}) => {
+        let listening = false
+        const listeners = []
+        const onChange = () => {
+            const value = get()
+            listeners.forEach((listener) => listener(value))
+        }
+        return {
+            get,
+            set: value => {
+                set(value)
+                onChange()
+            },
+            listen: (listener) => {
+                listeners.push(listener)
+                listener(get())  // send current value
+                if (!listening) {
+                    listen(onChange)
+                    listening = true
+                }
+            },
+        }
+    }
+
+    return {
+        make,
+        fromInputValue: (input) => make({
+            get: () => input.value,
+            set: (value) => { input.value = value },
+            listen: (listener) => input.addEventListener('change', listener),
+        }),
+        fromInputChecked: (input) => make({
+            get: () => input.checked,
+            set: (value) => { input.checked = value },
+            listen: (listener) => input.addEventListener('change', listener),
+        }),
+        map: (variable, mapping) => make({
+            get: mapping.getter(variable.get),
+            set: mapping.setter(variable.set),
+            listen: variable.listen,
+        }),
+    }
+}()
+
+function combineLatest(...listenables) {
+    let listening = false
+    const listeners = []
+
+    const waitingFor = new Set(Array(listenables.length).keys())
+    const latest = []
+    return {
+        listen: (listener) => {
+            listeners.push(listener)
+            if (!listening) {
+                listenables.forEach((listenable, idx) => listenable.listen((value) => {
+                    latest[idx] = value
+                    waitingFor.delete(idx)
+                    if (waitingFor.size === 0) {
+                        listeners.forEach(listener => listener([...latest]))
+                    }
+                }))
+                listening = true
+            }
+        }
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const passwordInput = document.getElementById('password');
-    const rememberPasswordInput = document.getElementById('remember-password');
-    const siteInput = document.getElementById('site');
-    const sitePasswordInput = document.getElementById('site-password');
+    const form = {
+        limit: document.getElementById('limit'),
+        password: document.getElementById('password'),
+        rememberPassword: document.getElementById('remember-secret'),
+        secret: document.getElementById('secret'),
+        subject: document.getElementById('subject'),
+    }
 
-    if (localStorage.getItem(passwordStorageKey) !== null) {
-        passwordInput.value = localStorage.getItem(passwordStorageKey);
-        rememberPasswordInput.checked = true;
-        siteInput.focus();
+    const controls = {
+        limit: variable.map(
+            variable.fromInputValue(form.limit),
+            {
+                getter: (get) => () => parseInt(get()),
+                setter: (set) => (value) => set(value.toString()),
+            }
+        ),
+        password: variable.fromInputValue(form.password),
+        rememberSecret: variable.fromInputChecked(form.rememberPassword),
+        secret: variable.fromInputValue(form.secret),
+        subject: variable.fromInputValue(form.subject),
+    }
+
+    loadValuesFromStorage(controls)
+
+    loadValuesFromURL(controls)
+
+    if (form.secret.value === "") {
+        form.secret.focus()
+    } else if (form.subject.value === "") {
+        form.subject.focus()
     } else {
-        passwordInput.focus();
+        form.password.focus()
     }
 
-    const params = new URLSearchParams(location.search);
-
-    const limitValue = parseInt(params.get('limit'));
-    if (isNaN(limitValue) && params.has('limit')) {
-        console.warn("'limit' parameter set to invalid value:", params.get('limit'));
-    }
-
-    const siteValue = params.get('site');
-    if (siteValue) {
-        siteInput.value = siteValue;
-        onChange();
-    }
-
-    function onChange() {
-        computeUniqpass(passwordInput.value, siteInput.value, limitValue).then(uniqpass => {
-            sitePasswordInput.value = uniqpass;
-            if (sitePasswordInput.value !== "") sitePasswordInput.select();
-        });
-    }
-
-    function rememberPassword() {
-        localStorage.setItem(passwordStorageKey, passwordInput.value);
-    }
-
-    function forgetPassword() {
-        localStorage.removeItem(passwordStorageKey);
-    }
-
-    passwordInput.addEventListener('change', () => {
-        if (rememberPasswordInput.checked) rememberPassword();
-        onChange();
-    });
-    siteInput.addEventListener('change', onChange);
-
-    rememberPasswordInput.addEventListener('change', () => {
-        if (rememberPasswordInput.checked) {
-            if (passwordInput.value !== "") rememberPassword();
+    combineLatest(controls.secret, controls.rememberSecret).listen(([secret, remember]) => {
+        if (remember) {
+            localStorage.setItem(secretStorageKey, secret)
         } else {
-            forgetPassword();
+            localStorage.removeItem(secretStorageKey)
         }
-    });
+    })
+
+    combineLatest(controls.secret, controls.subject, controls.limit).listen(([secret, subject, limit]) => {
+        computeUniqpass(secret, subject, limit).then((password) => {
+            controls.password.set(password)
+            if (controls.password.get() !== "") form.password.select()
+        })
+    })
+
+    combineLatest(controls.subject, controls.limit).listen(([subject, limit]) => {
+        const url = new URL(location)
+        const params = url.searchParams
+        if (subject !== "") {
+            params.set('subject', subject)
+        }
+        if (limit !== defaultLimit) {
+            params.set('limit', limit)
+        }
+        history.replaceState(null, "", url)
+    })
 })
